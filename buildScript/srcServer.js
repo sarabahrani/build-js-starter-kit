f65438ba5a591ec58fe0b435510a1288e001e5e8
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-09-04 21:44:28 -0700

    change it to es6 format,add webpack config

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -1,13 +1,20 @@
-var bs=require('browser-sync');
-bs.init({
-    server: "src",
-    files: ["src/style/*.css"],
-    plugins: [
-        {
-            module: "bs-html-injector",
-            options: {
-                files: ["src/*.html"]
-            }
-        }
+import browserSync from'browser-sync';
+import webpack from 'webPack';
+import webpackConfig from '../webpack.config.dev';
+import webpackDevMiddleware  from 'webpack-dev-middleware'
+let bundler = webpack(webpackConfig);
+browserSync.init({
+    server: 'src',
+    middleware: [
+        webpackDevMiddleware(bundler, {
+            publicPath: webpackConfig.output.publicPath,
+            stats: {colors: true} //to show info in console colorful
+        })
+    ],
+    plugins: [],
+    files: [
+        'src/style/*.css',
+        'src/*.html'
     ]
+    
 });
