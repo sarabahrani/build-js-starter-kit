d25a5f05259656f14e20281b2ec0cd85c9d34510
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-09-04 21:45:27 -0700

    add webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+import path from 'path';
+export default {
+    devtool: 'inline-source-map',
+    entry: [
+        path.resolve(__dirname, 'src/app.js')
+    ],
+    target: 'web',
+    output: {
+        path:path.resolve(__dirname, 'src'),
+        publicPath: '/', //directory or path in serevr to create the bundle
+        filename: 'bundle.js'
+    },
+    plugins: [],
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'] },
+            { test: /\.css$/, loader: ['css-loader'] }
+        ]
+    }
+}
