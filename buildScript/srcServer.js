05e7b5851c188b2875e5389c097b2a51efcb8fec
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-09-12 10:44:40 -0700

    add routing to server

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -1,20 +1,26 @@
-import browserSync from'browser-sync';
+import browserSync from 'browser-sync';
 import webpack from 'webPack';
 import webpackConfig from '../webpack.config.dev';
-import webpackDevMiddleware  from 'webpack-dev-middleware';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 let bundler = webpack(webpackConfig);
 browserSync.init({
-    server: 'src',
+    server: {
+        baseDir: "src",
+        routes: {
+            '/user': 'src/api/db.json'
+        }
+    },
+    browser:"chrome",
     middleware: [
         webpackDevMiddleware(bundler, {
             publicPath: webpackConfig.output.publicPath,
-            stats: {colors: true} //to show info in console colorful
-        })
+            stats: { colors: true } //to show info in console colorful
+        }), 
     ],
     plugins: [],
     files: [
         'src/style/*.css',
         'src/*.html'
     ]
-    
+
 });
